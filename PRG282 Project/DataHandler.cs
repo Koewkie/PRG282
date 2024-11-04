a559cbdb84a6c30706399cfe8f499eaeae5d5c47
Koewkie <darrat199@gmail.com>
2024-11-04 15:54:48 +0200

    Added DataHandler and FileHandler classes

---
+++ PRG282 Project/DataHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG282_Project
+{
+    internal class DataHandler
+    {
+        public DataHandler() { }
+
+        public List<Student> AddStudent(int iD, int age, string name, string course, List<Student> stdnList)
+        {
+            //-----------IMPLEMENT-------------------
+            return new List<Student>(); 
+        }
+
+        public List<Student> UpdateStudent(int iD, int age, string name, string course, List<Student> stdnList)
+        {
+            //-----------IMPLEMENT-------------------
+            return new List<Student>(); 
+        }
+
+        public List<Student> DeleteStudent(int iD, List<Student> stdnList)
+        {
+            //-----------IMPLEMENT-------------------
+            return new List<Student>();
+        }
+    }
+}
---
+++ PRG282 Project/FileHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG282_Project
+{
+    internal class FileHandler
+    {
+        private string path = "students.txt";
+
+        public FileHandler() { }
+
+        public List<Student> Read()
+        {
+            //-----------IMPLEMENT-------------------
+            return new List<Student>(); 
+        }
+
+        public void Write(List<Student> stdnList)
+        {
+            //-----------IMPLEMENT-------------------
+        }
+
+        public void GenerateSummary(List<Student> stdnList)
+        {
+            //-----------IMPLEMENT-------------------
+        }
+    }
+}
